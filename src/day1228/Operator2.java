c534198943e2880d82efaae246995c99ad527dc5
ljhee92 <weilainib7@gmail.com>
2024-01-06 22:09:56 +0900

    operator 2

--- src/day1228/Operator2.java
+++ src/day1228/Operator2.java
@@ -13,6 +13,20 @@ public class Operator2 {
 		i = 11;
 		System.out.println(i + "를 2로 나눈 나머지: " + (i % 2));	// 0 또는 1
 
+		
+		int birth = 1992;
+		System.out.println(birth + "를 12로 나눈 나머지: " + (birth%12));
+		// 0-원숭이, 1-닭, 2-개, 3-돼지, 4-쥐, 5-소, 6-호랑이, 7-토끼, 8-용, 9-뱀, 10-말, 11-양
+		
+		
+		// 정수 / 정수 = 정수
+		int num = 10, num2 = 3;
+		System.out.println(num + " / " + num2 + " = " + (num/num2));
+		
+		
+		// 정수 / 실수 = 실수
+		// 연산 결과를 실수로 얻으려면 나누는 수 중 하나는 실수여야 한다.
+		System.out.println(num + " / " + num2 + " = " + (num / (double)num2));
 
 	}	// main
 
