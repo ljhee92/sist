de5459e2b18124a05979bfc6d1aa5e9993586cbd
ljhee92 <weilainib7@gmail.com>
2024-01-06 12:39:11 +0900

    commit test

--- src/day1228/Operator2.java
+++ src/day1228/Operator2.java
@@ -10,6 +10,9 @@ public class Operator2 {
 		
 		int i = 10;
 		System.out.println(i + "를 2로 나눈 나머지: " + (i % 2));	// 0 또는 1
+		i = 11;
+		System.out.println(i + "를 2로 나눈 나머지: " + (i % 2));	// 0 또는 1
+
 
 	}	// main
 
