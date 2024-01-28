8cde2cc9abcdfccf0806a4b47cfe58edbb181e18
ljhee92 <weilainib7@gmail.com>
2024-01-28 19:10:34 +0900

    day1226

---
+++ src/day1226/Hello.java
@@ -0,0 +1,21 @@
+package day1226;
+
+/**
+ * 소스코드 첫 작성
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("안녕하세요?");
+		System.out.println("^0^ 포항항항");
+		
+		// 1. 소스코드 작성: 대소문자 구분, 괄호는 짝으로, 문장의 끝에는 ;을 붙임.
+		// 2. 저장: 클래스명.java (Hello.java)
+		// 3. 컴파일: compiler 필요 (javac.exe) javac 소스파일명.java
+		// 4. 실행: 실행기 필요 (java.exe) java bytecode명 
+
+	}	// main
+
+}	// class
---
+++ src/day1226/Test.java
@@ -0,0 +1,16 @@
+package day1226;
+
+/**
+ * 출력 테스트 
+ */
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이름: 이주희");
+		System.out.println("주소: 서울시 관악구");
+
+	}	// main
+
+}	// class
