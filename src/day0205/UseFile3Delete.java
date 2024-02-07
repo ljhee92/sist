3af7e4e9bba6d23e1b407d638f1935c1f30b922e
ljhee92 <weilainib7@gmail.com>
2024-02-07 20:42:57 +0900

    day0205 change path

--- src/day0205/UseFile3Delete.java
+++ src/day0205/UseFile3Delete.java
@@ -7,16 +7,16 @@ public class UseFile3 {
 	public static void main(String[] args) {
 		
 		// 1. 삭제할 파일명을 가진 File 객체를 생성
-//		File file = new File("/Users/juhee/eclipse-workspace/sist/temp/java_read2.rtf");
+//		File file = new File("/Users/juhee/eclipse-workspace/sist/temp/java_read2.txt");
 		
 		// 2. 삭제
 //		System.out.println(file.delete());
 		
 		// 1. 이전 이름을 가진 파일 객체를 생성
-		File file = new File("/Users/juhee/eclipse-workspace/sist/temp/java_read2.rtf");
+		File file = new File("/Users/juhee/eclipse-workspace/sist/temp/java_read2.txt");
 		
 		// 2. 변경할 파일 명을 저장한 객체 생성
-		File file2 = new File("/Users/juhee/eclipse-workspace/sist/temp/jh.rtf");
+		File file2 = new File("/Users/juhee/eclipse-workspace/sist/temp/jh.txt");
 		
 		// 3. 변경
 		System.out.println(file.renameTo(file2));
